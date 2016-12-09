f050a678f4d8af545d4939c940a3e4c13f4e77f7
vipinanandcpp <vipin.anand.cpp@gmail.com>
2016-12-09 00:00:09 -0500

    jose code

--- src/NAFTA4Vipin/daily_alert3_clock.py
+++ src/NAFTA4Vipin/daily_alert3_clock.py
@@ -92,46 +92,46 @@ palabras=['the border','manufacturing','remmitance','carrier','indiana','indiana
 # In[4]:
 
 def prepare_html(liston,color):
-    liston_out=list()
-    for headline,url in liston:
-        if(len(headline)>0):
-            out1_jose = "<tr><td BGCOLOR="+color+">" + headline + "<td align=center>" + url +  "</tr>";
-            #out1_jose = "<tr><td BGCOLOR=#F7BE81>" + headline + "<td align=center>" + url +  "</tr>";
-            liston_out.append(out1_jose)
-    return liston_out
+	liston_out=list()
+	for headline,url in liston:
+		if(len(headline)>0):
+			out1_jose = "<tr><td BGCOLOR="+color+">" + headline + "<td align=center>" + url +  "</tr>";
+			#out1_jose = "<tr><td BGCOLOR=#F7BE81>" + headline + "<td align=center>" + url +  "</tr>";
+			liston_out.append(out1_jose)
+	return liston_out
 
 
 # In[5]:
 
 def prepare_wsj():
-    pq = PyQuery('http://www.wsj.com/news/heard-on-the-street')
+	pq = PyQuery('http://www.wsj.com/news/heard-on-the-street')
 
-    base_url = 'http://www.wsj.com/'
+	base_url = 'http://www.wsj.com/'
 
-    blog_url='http://blogs.wsj.com/'
+	blog_url='http://blogs.wsj.com/'
 
-    urls = []
+	urls = []
 
 
-    for item in pq('.headline-container .headline a').items():
-        href  = item.attr['href']
-        if base_url in href:
-            urls.append(href)
-        else:
-            if blog_url not in href:
-                urls.append(base_url+href)
-            else:
-                urls.append(href)
+	for item in pq('.headline-container .headline a').items():
+		href  = item.attr['href']
+		if base_url in href:
+			urls.append(href)
+		else:
+			if blog_url not in href:
+				urls.append(base_url+href)
+			else:
+				urls.append(href)
 
-    #url_processor = URLProcessor()
+	#url_processor = URLProcessor()
 
-    liston_wsj=list()
+	liston_wsj=list()
 
-    for elem in urls:
-        article=url_processor.process_url(elem)
-        liston_wsj.append((article.title,elem))
+	for elem in urls:
+		article=url_processor.process_url(elem)
+		liston_wsj.append((article.title,elem))
 
-    return liston_wsj
+	return liston_wsj
 
 
 
@@ -140,122 +140,122 @@ def prepare_wsj():
 # In[6]:
 
 def prepare_bb():
-    pq = PyQuery('http://www.bloomberg.com')
+	pq = PyQuery('http://www.bloomberg.com')
 
-    base_url = 'http://www.bloomberg.com/'
+	base_url = 'http://www.bloomberg.com/'
 
-    urls = []
-
-
-    for item in pq('.hero-v6-story__info-container .hero-v6-story__info a').items():
-        href  = item.attr['href']
-        if base_url in href:
-            urls.append(href)
-        else:
-            urls.append(base_url+href)
-
-    liston_bb=list()
+	urls = []
 
 
-    for elem in urls:
-        article=url_processor.process_url(elem)
-        liston_bb.append((article.title,elem))
+	for item in pq('.hero-v6-story__info-container .hero-v6-story__info a').items():
+		href  = item.attr['href']
+		if base_url in href:
+			urls.append(href)
+		else:
+			urls.append(base_url+href)
 
-    return liston_bb
+	liston_bb=list()
+
+
+	for elem in urls:
+		article=url_processor.process_url(elem)
+		liston_bb.append((article.title,elem))
+
+	return liston_bb
 
 
 # In[7]:
 
 def prepare_cnn():
-    pq = PyQuery("http://money.cnn.com/news/economy/")
+	pq = PyQuery("http://money.cnn.com/news/economy/")
 
 
-    #
-    base_url = 'http://money.cnn.com/'
+	#
+	base_url = 'http://money.cnn.com/'
 
 
-    urls = []
+	urls = []
 
 
-    for item in pq('article  a').items():
-        href  = item.attr['href']
-        if base_url in href:
-            urls.append(href)
-        else:
-            urls.append(base_url+href)
+	for item in pq('article  a').items():
+		href  = item.attr['href']
+		if base_url in href:
+			urls.append(href)
+		else:
+			urls.append(base_url+href)
 
-    talla=len(urls)
+	talla=len(urls)
 
-    limite=min(talla,5)
+	limite=min(talla,5)
 
-    liston_cnn=list()
+	liston_cnn=list()
 
 
 
-    for elem in urls[0:limite]:
-        article=url_processor.process_url(elem)
-        liston_cnn.append((article.title,elem))
+	for elem in urls[0:limite]:
+		article=url_processor.process_url(elem)
+		liston_cnn.append((article.title,elem))
 
-    return liston_cnn
+	return liston_cnn
 
 
 # In[8]:
 
 def prepare_bi():
-    pq = PyQuery("http://www.businessinsider.com.au/category/10-things-before-opening-bell")
+	pq = PyQuery("http://www.businessinsider.com.au/category/10-things-before-opening-bell")
 
 
-    #
-    base_url = 'http://www.businessinsider.com.au/'
+	#
+	base_url = 'http://www.businessinsider.com.au/'
 
 
-    urls = []
+	urls = []
 
 
-    for item in pq('.post-description .post-title a').items():
-        href  = item.attr['href']
-        if base_url in href:
-            urls.append(href)
-        else:
-            urls.append(base_url+href)
+	for item in pq('.post-description .post-title a').items():
+		href  = item.attr['href']
+		if base_url in href:
+			urls.append(href)
+		else:
+			urls.append(base_url+href)
 
-    talla=len(urls)
+	talla=len(urls)
 
-    limite=min(talla,2)
+	limite=min(talla,2)
 
-    liston_bi=list()
+	liston_bi=list()
 
 
 
-    for elem in urls[0:limite]:
-        article=url_processor.process_url(elem)
-        liston_bi.append((article.title,elem))
+	for elem in urls[0:limite]:
+		article=url_processor.process_url(elem)
+		liston_bi.append((article.title,elem))
 
-    return liston_bi
+	return liston_bi
 
 
 # In[9]:
 
 def prepare_financiero():
-    urls=[]
-    texto=[]
-    base_url = 'http://www.elfinanciero.com.mx'
-    pq = PyQuery('http://www.elfinanciero.com.mx/economia/')
+	urls=[]
+	texto=[]
+	base_url = 'http://www.elfinanciero.com.mx'
+	pq = PyQuery('http://www.elfinanciero.com.mx/economia/')
 
-    for item in pq('.category-main-news .title a'):
-        #print item.text
-        href  = item.attrib['href']
-        if base_url in href:
-            urls.append(href)
-            texto.append(item.text)
-        else:
-            urls.append(base_url+href)
-            texto.append(item.text)
-    liston_financiero=list()
+	for item in pq('.category-main-news .title a'):
+		#print item.text
+		href  = item.attrib['href']
+		if base_url in href:
+			urls.append(href)
+			texto.append(item.text)
+		else:
+			urls.append(base_url+href)
+			texto.append(item.text)
+	liston_financiero=list()
 
-    for aux  in range(len(urls)):
-        liston_financiero.append((texto[aux],urls[aux]))
-    return  liston_financiero
+	for aux  in range(len(urls)):
+		liston_financiero.append((texto[aux],urls[aux]))
+	return  liston_financiero
 
 
 
@@ -267,82 +267,82 @@ def prepare_financiero():
 # In[12]:
 
 def prepare_universal():
-    urls=[]
-    urls=[]
-    texto=[]
-    base_url='http://www.eluniversal.com.mx'
-    pq = PyQuery('http://www.eluniversal.com.mx/cartera/economia/')
+	urls=[]
+	urls=[]
+	texto=[]
+	base_url='http://www.eluniversal.com.mx'
+	pq = PyQuery('http://www.eluniversal.com.mx/cartera/economia/')
 
-    #for item in pq(' .field-content a'):
-    for item in pq('.field-content a'):
-        href  = item.attrib['href']
-        if href is None:
-            continue
-        if item.text is None:
-            continue
-        if len(item.text)<10:
-            continue
+	#for item in pq(' .field-content a'):
+	for item in pq('.field-content a'):
+		href  = item.attrib['href']
+		if href is None:
+			continue
+		if item.text is None:
+			continue
+		if len(item.text)<10:
+			continue
 
-        if base_url in href:
-            urls.append(href)
-            texto.append(item.text)
-        else:
-            urls.append(base_url+href)
-            texto.append(item.text)
+		if base_url in href:
+			urls.append(href)
+			texto.append(item.text)
+		else:
+			urls.append(base_url+href)
+			texto.append(item.text)
 
-    liston_universal=list()
+	liston_universal=list()
 
-    for aux  in range(len(urls)):
-        liston_universal.append((texto[aux],urls[aux]))
-    return  liston_universal
+	for aux  in range(len(urls)):
+		liston_universal.append((texto[aux],urls[aux]))
+	return  liston_universal
 
 def gimma_donald_df():
-    today=datetime.datetime.now() - timedelta(days=5)
-    today=today.date()
-    names=['time','text']
-    path1=os.path.join(settings.src_files, 'NAFTA4Vipin','stored_accounts')
-    username='RealDonaldTrump'
-    fName=path1+"/"+username+".txt"
-    if not path.exists(fName):
-        return None
+	today=datetime.datetime.now() - timedelta(days=5)
+	today=today.date()
+	names=['time','text']
+	path1=os.path.join(settings.src_files, 'NAFTA4Vipin','stored_accounts')
+	username='RealDonaldTrump'
+	fName=path1+"/"+username+".txt"
+	if not path.exists(fName):
+		return None
 #names=['Company','Last Sale','Change','Net%','Share Volume','News','url']
-    pos_out=pd.DataFrame(columns=names)
-    with open(fName) as f:
-            for line in f:
-                result = jsonpickle.decode(line)
-                id_=result['id']
-                pp=result['created_at']
-                ff=datetime.datetime.strptime(pp,'%a %b %d %H:%M:%S +0000 %Y')
-                gg=utc.localize(ff).astimezone(eastern)
-                if gg.date()>=today:
-                    #print result['text'],ff,gg,id_,gg.strftime("%Y-%m-%d %H:%M:%S %Z")
-                    pos_out.loc[id_]=[gg.strftime("%Y-%m-%d %H:%M:%S %Z"),result['text']]
+	pos_out=pd.DataFrame(columns=names)
+	with open(fName) as f:
+			for line in f:
+				result = jsonpickle.decode(line)
+				id_=result['id']
+				pp=result['created_at']
+				ff=datetime.datetime.strptime(pp,'%a %b %d %H:%M:%S +0000 %Y')
+				gg=utc.localize(ff).astimezone(eastern)
+				if gg.date()>=today:
+					#print result['text'],ff,gg,id_,gg.strftime("%Y-%m-%d %H:%M:%S %Z")
+					pos_out.loc[id_]=[gg.strftime("%Y-%m-%d %H:%M:%S %Z"),result['text']]
 
 
 
-    pos_out.sort_index(inplace=True,ascending=False)
+	pos_out.sort_index(inplace=True,ascending=False)
 
-    return pos_out
+	return pos_out
 
 
 def prepare_donald():
-    donald_df=gimma_donald_df()
+	donald_df=gimma_donald_df()
 
-    if donald_df is None:
-        return None
-    else:
-        source="Donald Trump at "
-        liston_donald=list()
-        limite=5
-        contador=0
-        for idx,row in donald_df.iterrows():
-            if any(t in  row['text'].lower() for t in palabras):
-                #print row['text']+" at "+row['time']
-                liston_donald.append((row['text'],source+row['time']))
-                if contador>limite:
-                    break
-                contador=contador+1
-        return liston_donald
+	if donald_df is None:
+		return None
+	else:
+		source="Donald Trump at "
+		liston_donald=list()
+		limite=5
+		contador=0
+		for idx,row in donald_df.iterrows():
+			if any(t in  row['text'].lower() for t in palabras):
+				#print row['text']+" at "+row['time']
+				liston_donald.append((row['text'],source+row['time']))
+				if contador>limite:
+					break
+				contador=contador+1
+		return liston_donald
 
 
 
@@ -356,17 +356,17 @@ def prepare_donald():
 # In[13]:
 
 def prepare_big_csv(big_liston):
-    path=os.path.join(settings.src_files, 'NAFTA4Vipin','stored_news')
-    today=datetime.datetime.now().date()
-    df_out=pd.DataFrame(columns=['text','url'])
-    contador=0
-    for agency in big_liston:
-        for news in agency:
-            df_out.loc[contador]=news
-            contador=contador+1
+	path=os.path.join(settings.src_files, 'NAFTA4Vipin','stored_news')
+	today=datetime.datetime.now().date()
+	df_out=pd.DataFrame(columns=['text','url'])
+	contador=0
+	for agency in big_liston:
+		for news in agency:
+			df_out.loc[contador]=news
+			contador=contador+1
 
-    df_out.to_csv(path+"/"+"TOP_NEWS_"+today.strftime('%m%d%Y')+".csv",encoding='utf-8')
-    print ("news done")
+	df_out.to_csv(path+"/"+"TOP_NEWS_"+today.strftime('%m%d%Y')+".csv",encoding='utf-8')
+	print ("news done")
 
 
 
@@ -376,57 +376,57 @@ def prepare_big_csv(big_liston):
 # In[17]:
 
 def prepare_big_html(big_liston,color):
-    total = "<html><body><table border=5 BORDERCOLOR=BLUE><CAPTION><b><u>  NAFTA INSIGHT 1345 </u></b></CAPTION>";
-    total = total + "<tr><td><b>" + "TITLE" + "</b><td width='230' align=center><b>"  + "URL" + "</b></tr>";
-    tempo=prepare_html(big_liston[0],color[0])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
-    tempo=prepare_html(big_liston[1],color[1])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
-    tempo=prepare_html(big_liston[2],color[2])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
+	total = "<html><body><table border=5 BORDERCOLOR=BLUE><CAPTION><b><u>  NAFTA INSIGHT 1345 </u></b></CAPTION>";
+	total = total + "<tr><td><b>" + "TITLE" + "</b><td width='230' align=center><b>"  + "URL" + "</b></tr>";
+	tempo=prepare_html(big_liston[0],color[0])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
+	tempo=prepare_html(big_liston[1],color[1])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
+	tempo=prepare_html(big_liston[2],color[2])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
 
-    tempo=prepare_html(big_liston[3],color[3])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
-
-
-    tempo=prepare_html(big_liston[4],color[4])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
-
-    tempo=prepare_html(big_liston[5],color[5])
-    if  len(tempo)>0:
-        for elem in tempo:
-            total=total+elem
+	tempo=prepare_html(big_liston[3],color[3])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
 
 
+	tempo=prepare_html(big_liston[4],color[4])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
 
-    total = total + "</table></body></html>";
+	tempo=prepare_html(big_liston[5],color[5])
+	if  len(tempo)>0:
+		for elem in tempo:
+			total=total+elem
 
-    return total
+
+
+	total = total + "</table></body></html>";
+
+	return total
 
 def prepare_big_html_new(big_liston,color):
-    total = "<html><body><table border=5 BORDERCOLOR=BLUE><CAPTION><b><u>  NAFTA INSIGHT 1345 </u></b></CAPTION>";
-    total = total + "<tr><td><b>" + "TITLE" + "</b><td width='230' align=center><b>"  + "SOURCE" + "</b></tr>";
+	total = "<html><body><table border=5 BORDERCOLOR=BLUE><CAPTION><b><u>  NAFTA INSIGHT 1345 </u></b></CAPTION>";
+	total = total + "<tr><td><b>" + "TITLE" + "</b><td width='230' align=center><b>"  + "SOURCE" + "</b></tr>";
 
-    for kk in range(len(big_liston)):
-        tempo=prepare_html(big_liston[kk],color[kk])
-        if  len(tempo)>0:
-            for elem in tempo:
-                total=total+elem
+	for kk in range(len(big_liston)):
+		tempo=prepare_html(big_liston[kk],color[kk])
+		if  len(tempo)>0:
+			for elem in tempo:
+				total=total+elem
 
 
-    total = total + "</table></body></html>";
+	total = total + "</table></body></html>";
 
-    return total
+	return total
 
 
 
@@ -460,8 +460,9 @@ def send_email_new(users,total):
 	# the HTML message, is best and preferred.
 	msg.attach(part1)
 	msg.attach(part2)
-	s.sendmail(me,[me]+[me]+ you.split(";"), msg.as_string().encode('ascii'))
-	s.quit()
+	print msg
+	#s.sendmail(me,[me]+[me]+ you.split(";"), msg.as_string().encode('ascii'))
+	#s.quit()
 	print('email sent.')
 
 
@@ -471,34 +472,34 @@ def send_email_new(users,total):
 @sched.scheduled_job('cron', hour=8,minute=45,misfire_grace_time=60)
 def timed_job():
 # In[3]:
-    liston_donald=prepare_donald()
-    liston_bi=prepare_bi()
-    liston_wsj=prepare_wsj()
-    liston_bb=prepare_bb()
-    liston_cnn=prepare_cnn()
-    liston_financiero=prepare_financiero()
-    liston_universal=prepare_universal()
+	liston_donald=prepare_donald()
+	liston_bi=prepare_bi()
+	liston_wsj=prepare_wsj()
+	liston_bb=prepare_bb()
+	liston_cnn=prepare_cnn()
+	liston_financiero=prepare_financiero()
+	liston_universal=prepare_universal()
 
 
 
 
-    if liston_donald is  None:
-        big_liston=[liston_universal,liston_financiero,liston_bi,liston_cnn,liston_bb,liston_wsj]
-        color=['#809ff7','#f780e3','#ff9900','#00FF00','#00FFFF','#F3F781']
-    else:
-        big_liston=[liston_donald,liston_universal,liston_financiero,liston_bi,liston_cnn,liston_bb,liston_wsj]
-        color=['#aa9a06','#809ff7','#f780e3','#ff9900','#00FF00','#00FFFF','#F3F781']
+	if liston_donald is  None:
+		big_liston=[liston_universal,liston_financiero,liston_bi,liston_cnn,liston_bb,liston_wsj]
+		color=['#809ff7','#f780e3','#ff9900','#00FF00','#00FFFF','#F3F781']
+	else:
+		big_liston=[liston_donald,liston_universal,liston_financiero,liston_bi,liston_cnn,liston_bb,liston_wsj]
+		color=['#aa9a06','#809ff7','#f780e3','#ff9900','#00FF00','#00FFFF','#F3F781']
 
 
-    total=prepare_big_html_new(big_liston,color)
+	total=prepare_big_html_new(big_liston,color)
 
-    #users="jose.pereza@me.com;vipin.anand.cpp@gmail.com;salumgreco@hotmail.com"
+	#users="jose.pereza@me.com;vipin.anand.cpp@gmail.com;salumgreco@hotmail.com"
 
-    #users='egreenfield@121send.com;Raphael_Kuenstle@gmx.net;jmcanchola@gmail.com;apina1411@gmail.com;jose.pereza@me.com;vipin.anand.cpp@gmail.com;japerez20@gmail.com;juanmanuelhec@gmail.com;salumgreco@hotmail.com;jtmaclay@yahoo.com'
-    users = 'jose.pereza@me.com;vipin.anand.cpp@gmail.com'
+	#users='egreenfield@121send.com;Raphael_Kuenstle@gmx.net;jmcanchola@gmail.com;apina1411@gmail.com;jose.pereza@me.com;vipin.anand.cpp@gmail.com;japerez20@gmail.com;juanmanuelhec@gmail.com;salumgreco@hotmail.com;jtmaclay@yahoo.com'
+	users = 'jose.pereza@me.com;vipin.anand.cpp@gmail.com'
 
-    send_email_new(users,total)
-    prepare_big_csv(big_liston)
+	send_email_new(users,total)
+	prepare_big_csv(big_liston)
 
 
 # In[ ]:
